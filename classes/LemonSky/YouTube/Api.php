fa222f98f28c15be8ab5d2b3a3eb016be9ef1bdf
Łukasz Leśko <lukaszlesko@gmail.com>
2013-09-17 13:51:38 +0200

    upload class

---
+++ classes/LemonSky/YouTube/Api.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace \LemonSky\YouTube;
+
+class Api
+{
+    protected $_yt;
+    protected $_debug;
+    
+    public function __construct()
+    {
+        $yt = new \ZendGData\YouTube();
+        $yt->setMajorProtocolVersion(2);
+        $yt->getHttpClient()->setOptions(array('sslverifypeer' => false));
+        
+        $this->_yt = $yt;
+    }
+    
+    public function setDebug($debug)
+    {
+        $this->_debug = $debug;
+    }
+    
+    public function getDebug()
+    {
+        return $this->_debug;
+    }
+    
+    public function collectVideos($channel, $maxPages = 41)
+    {
+        // user uploads
+        $location = 'https://gdata.youtube.com/feeds/api/users/' . $channel . '/uploads';
+        
+        // fetch all pages
+        $startIndex = 1;
+        $youtubePerPage = 25;
+        
+        $page = 0;
+        $videos = array();
+        
+        while ($page < $maxPages) {
+            $url = $location . '?start-index=' . $startIndex . '&key=' . $this->getDeveloperKey();
+            $uploads = $this->_yt->getUserUploads(null, $url);
+            
+            if ($this->getDebug()) {
+                echo sprintf("COLLECT VIDEOS, URL: %s, VIDEOS NB: %d\n", $url, count($uploads));
+            }
+            
+            if (count($uploads) == 0) {
+                break;
+            }
+        
+            foreach ($uploads as $video) {
+                if ($video) {
+                    $videoId = end(explode(":", $video->id->text));
+                    
+                    if (mb_strpos($videoId, '/') !== false) {
+                        $videoId = end(explode("/", $video->id->text));
+                    }
+                    
+                    $comments = !empty($video->comments->feedLink->countHint) ? $video->comments->feedLink->countHint : 0;
+                    
+                    $videos[] = array('id' => $videoId, 'comments' => $comments);
+                }
+            }
+        
+            $startIndex += $youtubePerPage;
+            $page++;
+        }
+    }
+    
+    public function collectCommentsForAllVideos($channel)
+    {
+        $videos = $this->collectVideos($channel);
+        
+        $comments = array();
+        
+        foreach ($videos as $video) {
+            try {
+                $comments[$video['id']] = $this->collectCommentsForVideo($channel, $video['id']);
+            } catch (Exception $e) {
+                echo 'error ' . $e->getMessage();
+            }
+        }
+        
+        return $comments;
+    }
+    
+    public function collectCommentsForVideo($channel, $videoId, $maxPages = 41)
+    {
+        $startIndex = 1;
+        $youtubePerPage = 25;
+        $page = 0;
+        
+        $comments = array();
+        
+        while ($page < $maxPages) {
+            $url = 'https://gdata.youtube.com/feeds/api/videos/' . $videoId . '/comments?start-index=' . $startIndex . '&key=' . $this->getDeveloperKey();
+            $commentFeed = $this->_yt->getVideoCommentFeed(null, $url);
+            
+            if ($this->getDebug()) {
+                echo sprintf("COLLECT COMMENTS, VIDEO: %s, COMMENTS NB: %d, URL: %s\n", $videoId, count($commentFeed), $url);
+            }
+            
+            if (count($commentFeed) == 0) {
+                break;
+            }
+            
+            foreach ($commentFeed as $commentEntry) {
+                $entry = array(
+                    'youtube_id' => $commentEntry->id->text,
+                    'comment' => $commentEntry->title->text,
+                    'comment_full' => $commentEntry->content->text,
+                    'author' => end(explode("/", $commentEntry->author[0]->uri->text)),
+                    'youtube_date' => date('Y-m-d H:i:s', strtotime($commentEntry->updated->text)),
+                    'created_at' => date('Y-m-d H:i:s')
+                );
+                
+                $comments[] = $entry;
+            }
+        
+            $startIndex += $youtubePerPage;
+            $page++;
+        }
+        
+        return $comments;
+    }
+}
---
+++ classes/LemonSky/YouTube/Upload.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace \LemonSky\YouTube;
+
+class Upload
+{
+    protected $_yt;
+    
+    public function __construct()
+    {
+        if (null === $config) {
+            $config = Kohana::$config->load('youtube');
+        }
+    
+        $httpClient = \ZendGdata\ClientLogin::getHttpClient(
+			$username = $config['username'],
+            $password = $config['password'],
+            $service = 'youtube',
+            $client = null,
+            $source = $config['source'],
+            $loginToken = null,
+            $loginCaptcha = null,
+            $config['auth_url']
+        );
+    
+        $developerKey = $config['dev_key'];
+        $applicationId = $config['source'];
+        $clientId = $config['source'];
+    
+        $this->_yt = new \ZendGdata\YouTube($httpClient, $applicationId, $clientId, $developerKey);
+        $this->_yt->setMajorProtocolVersion(2);
+    }
+    
+    /**
+     * Uploads video as private on current logged YT user channel.
+     *
+     * Configuration data are availabe in youtube.php config file.
+     *
+     * @param string $filepath full path to file
+     * @return bool|array false in case of error, video_edit_url i video_id of video on success
+     */
+    public function uploadFileFromLocalStorage($filepath)
+    {
+        $filesource = $this->_yt->newMediaFileSource($filepath);
+        $filesource->setContentType('video/quicktime');
+        $filesource->setSlug(end(explode('/', $filepath)));
+        
+        $myVideoEntry = new \ZendGdata\YouTube\VideoEntry();
+        $myVideoEntry->setMediaSource($filesource);
+        $myVideoEntry->setVideoTitle('Video');
+        $myVideoEntry->setVideoDescription('My video');
+        $myVideoEntry->setVideoCategory('Entertainment');
+        $myVideoEntry->setVideoPrivate();
+        $myVideoEntry->SetVideoTags('entertainment');
+
+        $newEntry = $this->_yt->insertEntry($myVideoEntry, $config['upload_url'], 'Zend_Gdata_YouTube_VideoEntry');
+        $newEntry->setMajorProtocolVersion(2);
+    
+        return array(
+            'video_id' => $newEntry->getVideoId(),
+            'video_edit_url' => $newEntry->getEditLink()->getHref()
+        );
+    }
+    
+    public function markPublic($videoId, $videoEditUrl)
+    {
+        $videoEntry = $this->_yt->getVideoEntry($videoId);
+        $videoEntry->setVideoPublic();
+        $this->_yt->updateEntry($videoEntry, $videoEditUrl);
+    }
+    
+    public function markPrivate($videoId, $videoEditUrl)
+    {
+        $videoEntry = $this->_yt->getVideoEntry($videoId);
+        $videoEntry->setVideoPrivate();
+        $this->_yt->updateEntry($videoEntry, $videoEditUrl);
+    }
+}
